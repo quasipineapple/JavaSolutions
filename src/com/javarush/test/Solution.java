2e3904aa04d03404461b407646b12ab29ca4f995
quasipineapple <skvorcov_s@mail.ru>
2015-08-11 21:54:12 +0300

    Initial commit

---
+++ src/com/javarush/test/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Сергей on 11.08.2015.
+ */
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        //напишите тут ваш код
+        ArrayList<Zerg> lz = new ArrayList<Zerg>(10);
+
+
+
+    }
+
+    public static class Zerg
+    {
+        public String name;
+    }
+
+    public static class Protos
+    {
+        public String name;
+    }
+
+    public static class Terran
+    {
+        public String name;
+    }
+}
